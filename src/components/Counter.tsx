ded9820eca4acfe9d48b3cc7ebe5ea6f782f5f1c
kumeyuta <ku8.m.1.e2@gmail.com>
2023-09-23 01:13:40 +0900

    カウンターの修正

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,17 @@
 import { FC } from 'react';
 
+import { CounterProvider } from 'context/CounterContext';
 import { CounterButton } from './CounterButton';
 import { CounterResult } from './CounterResult';
 
 export const Counter: FC = () => {
   return (
-    <>
+    <CounterProvider>
       <CounterResult />
       <div className="flex items-center justify-center gap-[2em]">
         <CounterButton step={1} type="+" />
         <CounterButton step={1} type="-" />
       </div>
-    </>
+    </CounterProvider>
   );
 };
